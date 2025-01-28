a3688c7c51e14ff24fafbafbb9fa1bac2897994f
Dawid - MacOS <dawidstachowiczzz@gmail.com>
2025-01-28 01:31:44 +0100

    Added path validation and improved user interaction

--- backup_folder.sh
+++ backup_folder.sh
@@ -2,31 +2,34 @@
 
 echo "Put folder path to backup:"
 read BACKUP_FOLDER_PATH
-echo
-echo "Check your PATH $BACKUP_FOLDER_PATH (y/n):"
-read YES_NO
-YES_NO=$(echo "$YES_NO" | tr '[:upper:]' '[:lower:]') 
-echo
-BACKUP_DATE=$(date +'%d-%m-%Y-%R')
+if [ ! -d "$BACKUP_FOLDER_PATH" ]; then
+    echo "Wrong path"
+    else
 
-if [ "$YES_NO" = "y" ]; then
+        echo
+        echo "Check your PATH: $BACKUP_FOLDER_PATH (y/n):"
+        read YES_NO
+        YES_NO=$(echo "$YES_NO" | tr '[:upper:]' '[:lower:]') 
+        echo
+        BACKUP_DATE=$(date +'%d-%m-%Y-%R')
 
-    if  [ -d "$BACKUP_FOLDER_PATH" ]; then
-        echo "Directory exists, now we made some magic..."
-        FOLDER_NAME=$(basename "$BACKUP_FOLDER_PATH")
-        FULL_BACKUP_PATH="backup/$FOLDER_NAME/$FOLDER_NAME-$BACKUP_DATE"
-        if [ -d "$FULL_BACKUP_PATH" ]; then
-            echo "Backup already exists. Wait one minute before you create next backup."
+        if [ "$YES_NO" = "y" ]; then
+
+            if  [ -d "$BACKUP_FOLDER_PATH" ]; then
+                echo "Directory exists, now we made some magic..."
+                FOLDER_NAME=$(basename "$BACKUP_FOLDER_PATH")
+                FULL_BACKUP_PATH="backup/$FOLDER_NAME/$FOLDER_NAME-$BACKUP_DATE"
+                    if [ -d "$FULL_BACKUP_PATH" ]; then
+                    echo "Backup already exists. Wait one minute before you create next backup."
+                        else
+                            sudo mkdir -p /backup/$FOLDER_NAME/$FOLDER_NAME-$BACKUP_DATE
+                            echo "Done! /backup/$FOLDER_NAME/$FOLDER_NAME-$BACKUP_DATE"
+                    fi
+            fi
+
+        elif [ $YES_NO = "n" ]; then
+            echo "Canceled"
         else
-            sudo mkdir -p /backup/$FOLDER_NAME/$FOLDER_NAME-$BACKUP_DATE
-            echo "Done! /backup/$FOLDER_NAME/$FOLDER_NAME-$BACKUP_DATE"
-
-    else 
-        echo "Wrong path"
-    fi
-
-elif [ $YES_NO = "n" ]; then
-    echo "Canceled"
-else
-    echo "Invalid input. Please enter 'y' or 'n'"
+            echo "Invalid input. Please enter 'y' or 'n'"
+        fi
 fi
